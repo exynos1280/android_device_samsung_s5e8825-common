096643a03639f960e9988d24da1701682ca06d51
LuK1337 <priv.luk@gmail.com>
2025-08-09 11:09:53 +0200

    s5e8825-common: Remove vendor/lineage device framework matrix inclusion
    See: I78da6340f38b93fdc4c3694cb8f431f387d16c0d
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -137,10 +137,9 @@ BOARD_VENDOR_RAMDISK_FRAGMENTS := dlkm
 BOARD_VENDOR_RAMDISK_FRAGMENT.dlkm.KERNEL_MODULE_DIRS := top
 
 # Manifest
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml \
-    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 
