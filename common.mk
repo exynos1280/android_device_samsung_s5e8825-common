5e961b4d5731a308dc5ce1d592c92f5ca80c6b97
Majaahh <maja.blaszczak@proton.me>
2025-09-19 17:00:46 +0200

    s5e8825-common: Switch back to custom samsung health
    This reverts commit 52cfc302fbb1a561f74b73db20f8425ae09923dd.
    
    * Linux4 for some reason didn't like my change on gerrit and he decided to revert it..
    
    Change-Id: I507f1dcd8e4d493cf8e63c9ac7533221e3fed490
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -146,10 +146,8 @@ $(call soong_config_set,exynos_hwc,force_client_video,true)
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.samsung \
-    android.hardware.health-service.samsung-recovery
-
-$(call soong_config_set,samsungHealthVars,enable_suspend,false)
+    android.hardware.health-service.s5e8825 \
+    android.hardware.health-service.s5e8825-recovery
 
 # Init
 PRODUCT_PACKAGES += \
