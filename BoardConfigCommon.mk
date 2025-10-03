7fab7e5bd49535abfbbae131f5824488a94dbc30
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-10-03 18:55:31 +0200

    s5e8825-common: Remove `debug.sf.enable_hwc_vds`
    * 0 by default
    
    Change-Id: Icc2c2fc374c6bd049f778723aa0754ab9ca66521
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -164,7 +164,6 @@ include hardware/samsung_slsi-linaro/config/BoardConfig8825.mk
 
 # Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
-TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
