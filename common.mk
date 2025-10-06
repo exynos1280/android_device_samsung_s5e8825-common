56bf7b8bc7746bd4efcfb335e60d5da63ca1c799
Majaahh <maja.blaszczak@proton.me>
2025-10-06 20:52:23 +0200

    i hate m33
    Change-Id: I4b3502cdaccfcf5d470eb4a2d215b4a46344e1f3
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -196,9 +196,6 @@ $(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
 # Linker
 PRODUCT_PACKAGES += public.libraries.txt
 
-# Live Display
-PRODUCT_PACKAGES += vendor.lineage.livedisplay-service.samsung-exynos
-
 # Log Tag
 include $(COMMON_PATH)/configs/vendor_logtag.mk
 
