a4b7d4f904dfeafb2a03fb53561496c6c63d115c
Majaahh <maja.blaszczak@proton.me>
2025-10-31 21:41:33 +0100

    s5e8825-common: Reformat python scripts with ruff
    Change-Id: Ia47d25d4af5325137bd00e0e50420b9393c487ae
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- configs/releasetools.py
+++ configs/releasetools.py
@@ -14,82 +14,163 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import common
-import re
+
 
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
-  return
+    OTA_InstallEnd(info)
+    return
+
 
 def IncrementalOTA_InstallEnd(info):
-  info.input_zip = info.target_zip
-  OTA_InstallEnd(info)
-  return
+    info.input_zip = info.target_zip
+    OTA_InstallEnd(info)
+    return
+
 
 def AddImage(info, basename, dest):
-  data = info.input_zip.read("IMAGES/" + basename)
-  common.ZipWriteStr(info.output_zip, basename, data)
-  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+    data = info.input_zip.read('IMAGES/' + basename)
+    common.ZipWriteStr(info.output_zip, basename, data)
+    info.script.Print(
+        'Patching {} image unconditionally...'.format(dest.split('/')[-1])
+    )
+    info.script.AppendExtra(
+        'package_extract_file("%s", "%s");' % (basename, dest)
+    )
+
 
 def AddFirmwareImage(info, model, basename, dest, simple=False, offset=8):
-  if ("RADIO/%s_%s" % (basename, model)) in info.input_zip.namelist():
-    data = info.input_zip.read("RADIO/%s_%s" % (basename, model))
-    common.ZipWriteStr(info.output_zip, "firmware/%s/%s" % (model, basename), data);
-    info.script.Print("Patching {} image unconditionally...".format(basename.split('.')[0]));
-    if simple:
-      info.script.AppendExtra('package_extract_file("firmware/%s/%s", "%s");' % (model, basename, dest))
-    else:
-      size = info.input_zip.getinfo("RADIO/%s_%s" % (basename, model)).file_size
-      info.script.AppendExtra('assert(s5e8825.write_data_bt("firmware/%s/%s", "%s", %d, %d));' % (model, basename, dest, offset, size))
-      return size
-    return 0
+    if ('RADIO/%s_%s' % (basename, model)) in info.input_zip.namelist():
+        data = info.input_zip.read('RADIO/%s_%s' % (basename, model))
+        common.ZipWriteStr(
+            info.output_zip, 'firmware/%s/%s' % (model, basename), data
+        )
+        info.script.Print(
+            'Patching {} image unconditionally...'.format(
+                basename.split('.')[0]
+            )
+        )
+        if simple:
+            info.script.AppendExtra(
+                'package_extract_file("firmware/%s/%s", "%s");'
+                % (model, basename, dest)
+            )
+        else:
+            size = info.input_zip.getinfo(
+                'RADIO/%s_%s' % (basename, model)
+            ).file_size
+            info.script.AppendExtra(
+                'assert(s5e8825.write_data_bt("firmware/%s/%s", "%s", %d, %d));'
+                % (model, basename, dest, offset, size)
+            )
+            return size
+        return 0
+
 
 def AddModelImage(info, model, basename, dest):
-    if model not in info.input_zip.read("RADIO/models").decode("utf-8", errors="ignore").splitlines():
+    if (
+        model
+        not in info.input_zip.read('RADIO/models')
+        .decode('utf-8', errors='ignore')
+        .splitlines()
+    ):
         return
 
-    data = info.input_zip.read("RADIO/" + basename + ".img")
-    common.ZipWriteStr(info.output_zip, f"{basename}.img", data)
-    info.script.AppendExtra('ifelse(getprop("ro.boot.em.model") == "%s",' % (model))
-    info.script.Print("Patching {} image unconditionally...".format(basename.split('.')[0]));
-    info.script.AppendExtra('package_extract_file("%s.img", "%s");' % (basename, dest))
+    data = info.input_zip.read('RADIO/' + basename + '.img')
+    common.ZipWriteStr(info.output_zip, f'{basename}.img', data)
+    info.script.AppendExtra(
+        'ifelse(getprop("ro.boot.em.model") == "%s",' % (model)
+    )
+    info.script.Print(
+        'Patching {} image unconditionally...'.format(basename.split('.')[0])
+    )
+    info.script.AppendExtra(
+        'package_extract_file("%s.img", "%s");' % (basename, dest)
+    )
     info.script.AppendExtra(',"");')
 
+
 def OTA_InstallEnd(info):
-  AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
-  AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
-  AddImage(info, "vendor_boot.img", "/dev/block/by-name/vendor_boot")
-  AddModelImage(info, "SC-53C", "dtbo_jpn", "/dev/block/by-name/dtbo")
+    AddImage(info, 'dtbo.img', '/dev/block/by-name/dtbo')
+    AddImage(info, 'vbmeta.img', '/dev/block/by-name/vbmeta')
+    AddImage(info, 'vendor_boot.img', '/dev/block/by-name/vendor_boot')
+    AddModelImage(info, 'SC-53C', 'dtbo_jpn', '/dev/block/by-name/dtbo')
 
-  if "RADIO/models" in info.input_zip.namelist():
-    modelsIncluded = []
-    for model in info.input_zip.read("RADIO/models").decode('utf-8').splitlines():
-      if "RADIO/version_%s" % model in info.input_zip.namelist():
-        modelsIncluded.append(model)
-        version = info.input_zip.read("RADIO/version_%s" % model).decode('utf-8').splitlines()[0]
-        offset = 8
-        numImages = 0
-        info.script.AppendExtra('# Firmware update to %s for %s' % (version, model))
-        info.script.AppendExtra('ifelse (getprop("ro.boot.em.model") == "%s" &&' % model)
-        info.script.AppendExtra('s5e8825.verify_no_downgrade("%s") == "0" &&' % version)
-        info.script.AppendExtra('getprop("ro.boot.bootloader") != "%s",' % version)
-        info.script.AppendExtra('assert(s5e8825.mark_header_bt("/dev/block/by-name/bota", 0, 0, 0));')
-        for image in 'fld.bin', 'harx.bin', 'keystorage.bin', 'ldfw.img', 'sboot.bin', 'tzar.img', 'tzsw.img', 'uh.bin':
-          size = AddFirmwareImage(info, model, image, "/dev/block/by-name/bota", False, offset)
-          if size > 0:
-            numImages += 1
-            offset += size + 36 # header size
-        info.script.AppendExtra('assert(s5e8825.mark_header_bt("/dev/block/by-name/bota", 0, %d, 3142939818));' % numImages)
-        AddFirmwareImage(info, model, "modem.bin", "/dev/block/by-name/radio", True)
-        AddFirmwareImage(info, model, "modem_debug.bin", "/dev/block/by-name/cp_debug", True)
-        info.script.AppendExtra(',"");')
+    if 'RADIO/models' in info.input_zip.namelist():
+        modelsIncluded = []
+        for model in (
+            info.input_zip.read('RADIO/models').decode('utf-8').splitlines()
+        ):
+            if 'RADIO/version_%s' % model in info.input_zip.namelist():
+                modelsIncluded.append(model)
+                version = (
+                    info.input_zip.read('RADIO/version_%s' % model)
+                    .decode('utf-8')
+                    .splitlines()[0]
+                )
+                offset = 8
+                numImages = 0
+                info.script.AppendExtra(
+                    '# Firmware update to %s for %s' % (version, model)
+                )
+                info.script.AppendExtra(
+                    'ifelse (getprop("ro.boot.em.model") == "%s" &&' % model
+                )
+                info.script.AppendExtra(
+                    's5e8825.verify_no_downgrade("%s") == "0" &&' % version
+                )
+                info.script.AppendExtra(
+                    'getprop("ro.boot.bootloader") != "%s",' % version
+                )
+                info.script.AppendExtra(
+                    'assert(s5e8825.mark_header_bt("/dev/block/by-name/bota", 0, 0, 0));'
+                )
+                for image in (
+                    'fld.bin',
+                    'harx.bin',
+                    'keystorage.bin',
+                    'ldfw.img',
+                    'sboot.bin',
+                    'tzar.img',
+                    'tzsw.img',
+                    'uh.bin',
+                ):
+                    size = AddFirmwareImage(
+                        info,
+                        model,
+                        image,
+                        '/dev/block/by-name/bota',
+                        False,
+                        offset,
+                    )
+                    if size > 0:
+                        numImages += 1
+                        offset += size + 36  # header size
+                info.script.AppendExtra(
+                    'assert(s5e8825.mark_header_bt("/dev/block/by-name/bota", 0, %d, 3142939818));'
+                    % numImages
+                )
+                AddFirmwareImage(
+                    info, model, 'modem.bin', '/dev/block/by-name/radio', True
+                )
+                AddFirmwareImage(
+                    info,
+                    model,
+                    'modem_debug.bin',
+                    '/dev/block/by-name/cp_debug',
+                    True,
+                )
+                info.script.AppendExtra(',"");')
 
-    modelCheck = ""
-    for model in modelsIncluded:
-      if len(modelCheck) > 0:
-        modelCheck += ' || '
-      modelCheck += 'getprop("ro.boot.em.model") == "%s"' % model
-    if len(modelCheck) > 0:
-      info.script.AppendExtra('%s || abort("Unsupported model, not updating firmware!");' % modelCheck)
-  return
+        modelCheck = ''
+        for model in modelsIncluded:
+            if len(modelCheck) > 0:
+                modelCheck += ' || '
+            modelCheck += 'getprop("ro.boot.em.model") == "%s"' % model
+        if len(modelCheck) > 0:
+            info.script.AppendExtra(
+                '%s || abort("Unsupported model, not updating firmware!");'
+                % modelCheck
+            )
+    return
--- extract-files.py
+++ extract-files.py
@@ -8,13 +8,13 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
-from extract_utils.main import (
-    ExtractUtils,
-    ExtractUtilsModule,
-)
 from extract_utils.fixups_lib import (
     lib_fixups,
     lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
 )
 
 namespace_imports = [
