c8184752eb3f5aa1f06c646385b5089f23cf58a3
Majaahh <maja.blaszczak@proton.me>
2025-11-01 19:14:32 +0100

    s5e8825-common: init: Move audio related stuff to a separate init
    Change-Id: I5e3f7ef0e4f36dc4d599f9e4274fc7eac90c2802
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -60,6 +60,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_product_strategies.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_product_strategies.xml \
     frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_stream_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_stream_volumes.xml
 
+# Audio - Init
+PRODUCT_PACKAGES += init.s5e8825.audio.rc
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl:64 \
