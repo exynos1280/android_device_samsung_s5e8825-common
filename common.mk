898f50b902e88301543d67b52aed306a8441b15d
amruio <uioamr65@gmail.com>
2026-01-12 04:13:13 +0200

    s5e8825-common: Disable 10-bit C2 support
    Our MFC does not support 10-bit decoding/encoding.
    Not disabling this option causes C2 codecs to falsely report 10-bit capabilities,
    which may lead some players to error out.
    
    Signed-off-by: amruio <uioamr65@gmail.com>

--- common.mk
+++ common.mk
@@ -104,6 +104,8 @@ PRODUCT_PACKAGES += \
     codec2.vendor.base.policy \
     codec2.vendor.ext.policy
 
+$(call soong_config_set_bool,openmax,UNSUPPORT_10BIT,true)
+
 # ConfigStore
 PRODUCT_PACKAGES += disable_configstore
 
